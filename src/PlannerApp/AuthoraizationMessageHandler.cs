02621b93b0a5aa28cd6970fa7db69dce37b9a4bc
Mohammad Khoulani <mhmmdkholani@gmail.con=m>
2021-06-26 17:41:38 +0300

    Create the api response models and init the HrtpClient instance

---
+++ src/PlannerApp/AuthoraizationMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    public class AuthoraizationMessageHandler : DelegatingHandler
+    {
+        private readonly ILocalStorageService _storage;
+
+        public AuthoraizationMessageHandler(ILocalStorageService storage)
+        {
+            _storage = storage;
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+           if(await _storage.ContainKeyAsync("access_token"))
+            {
+                var token = await _storage.GetItemAsStringAsync("access_token");
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
---
+++ src/PlannetApp.Shared/Responses/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlannetApp.Shared.Responses
+{
+    public class ApiResponse
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T Value { get; set; }
+    }
+
+}
