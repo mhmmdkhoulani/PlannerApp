b6928c7fee61743b2dea6779d59df0873fbf681e
Mohammad Khoulani <mhmmdkholani@gmail.con=m>
2021-06-29 15:15:47 +0300

    Implement the Auth Layout & Design the Login Form

---
+++ src/PlannerApp/Components/Authentication/LoginForm.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannetApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Components
+{
+    public partial class LoginForm : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new LoginRequest(); 
+
+        public async Task LoginUserAsync()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/PlannetApp.Shared/Models/LoginRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlannetApp.Shared.Models
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [StringLength(6)]
+        public string Password { get; set; }
+    }
+}
